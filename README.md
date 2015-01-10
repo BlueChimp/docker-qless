c4efff09b8506c9153805b54a50785259decf82b
Ben Evans <ben@bensbit.co.uk>
2015-01-10 18:43:54 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# docker-qless
+
+Boot up [qless](https://github.com/seomoz/qless) in a container.
+
+Requires a Redis container link.
+
+## Licence
+
+MIT
---
+++ run.sh
@@ -0,0 +1,2 @@
+# --no-launch refers to the browser
+REDIS_URL="redis://$REDIS_PORT_6379_TCP_ADDR:$REDIS_PORT_6379_TCP_PORT" qless-web --foreground --no-launch
